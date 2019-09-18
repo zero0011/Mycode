f8c9880fd1a872c6e892b282f2fca55ce1dc7d89
zero0011 <2732237782@qq.com>
2019-09-18 17:19:06 +0800

    mvvm模型

---
+++ js/mvvm/proxy.js
@@ -0,0 +1,13 @@
+var proxy = new Proxy({} , {
+    get : function(obj , prop) {
+        console.log('设置get操作');
+        return obj[prop]
+    },
+    set: function (obj,prop,value) {
+        console.log('设置set 操作') ;
+        obj[prop] = value ;
+    }
+});
+
+proxy.time = 35 ;
+console.log(proxy.time) ; 
---
+++ js/mvvm/readme.md
@@ -0,0 +1,3 @@
+- MVVM 分层 , 理解 vue  更深入
+JS  传统开发 写点事件 DOM编程  AJAX 
+
